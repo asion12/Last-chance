11e29c7c0e830b32417b6242014c9cbae4793171
StraightBet <slupgames1025@gmail.com>
2022-11-08 22:51:00 +0900

    플레이어 소지 스킬 로드 개발중

--- second-poject/Assets/01. Scripts/Manager/UIManager.cs
+++ second-poject/Assets/01. Scripts/Manager/UIManager.cs
@@ -13,9 +13,15 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject battle_SkillScrollView;
     [SerializeField] private Text battle_PlayerElementsInfo;
     [SerializeField] private Text battle_PlayerStatsInfo;
+    [SerializeField] private GameObject PlayerSkillListContent;
+    [SerializeField] private GameObject PlayerSkillButtonPrefab;
+    private Player player;
+    private EventManager eventManager;
     void Start()
     {
-
+        player = FindObjectOfType<Player>();
+        eventManager = FindObjectOfType<EventManager>();
+        UIUpdate_PlayerSkillList();
     }
 
     // Update is called once per frame
@@ -28,6 +34,21 @@ public class UIManager : MonoBehaviour
         else
         {
             playerBattleUI.transform.gameObject.SetActive(false);
+        }
+    }
+
+    public void UIUpdate_PlayerSkillList()
+    {
+        for (int i = 0; i < player.skillList.Count; i++)
+        {
+            Debug.Log("Count is " + player.skillList.Count);
+            GameObject skillButton;
+            skillButton = Instantiate(PlayerSkillButtonPrefab);
+            skillButton.transform.SetParent(PlayerSkillListContent.transform);
+            List<SO_Skill> skills = player.skillList;
+            int idx = i;
+            skillButton.GetComponent<Button>().onClick.AddListener(() => eventManager.OnPlayerCastSkillSet(skills[idx]));
+            Debug.Log(i);
         }
     }
 }
