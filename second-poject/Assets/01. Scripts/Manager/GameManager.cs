f6ffe142068773bfb27cfd071d85133734891431
StraightBet <slupgames1025@gmail.com>
2022-12-06 15:17:09 +0900

    기타 수정

--- second-poject/Assets/01. Scripts/Manager/GameManager.cs
+++ second-poject/Assets/01. Scripts/Manager/GameManager.cs
@@ -34,7 +34,6 @@ public class GameManager : MonoBehaviour
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
-
         else
         {
             Cursor.visible = true;
