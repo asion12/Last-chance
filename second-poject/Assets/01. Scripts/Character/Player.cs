a0a6381c951e2df564503d78295e4bf94731a6d2
StraightBet <slupgames1025@gmail.com>
2022-12-22 07:22:27 +0900

    최종본 빌드 완료

--- second-poject/Assets/01. Scripts/Character/Player.cs
+++ second-poject/Assets/01. Scripts/Character/Player.cs
@@ -28,7 +28,7 @@ public class Player : Character
     public bool[] isSkillOverClockList = { false, };
 
     float cpIncreaseTimer = 0;
-    float cpIncreaseTimer_MAX = 0.75f;
+    float cpIncreaseTimer_MAX = 2f;
 
     float cpDecreaseTimer = 0;
     float cpDecreaseTimer_MAX = 1f;
